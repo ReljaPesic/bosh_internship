203f5445c6aa5f952de343f2ad1d84da56e872bd
Relja <relja.pesic@gmail.com>
2025-08-26 18:30:05 +0200

    fixed bugs regardin post methods

--- src/main/java/com/example/minishop/product/ProductController.java
+++ src/main/java/com/example/minishop/product/ProductController.java
@@ -3,6 +3,7 @@ package com.example.minishop.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -24,7 +25,7 @@ public class ProductController {
   }
 
   @PostMapping("/api/products")
-  public Product create(Product product) {
+  public Product create(@RequestBody Product product) {
     return productRepository.save(product);
   }
 
