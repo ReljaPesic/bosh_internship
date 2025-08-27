7e131ce30b29f4a8893c60e6480a0a1430ae31c6
Relja <relja.pesic@gmail.com>
2025-08-27 10:55:31 +0200

    added pagination/sorting/filering to the GET endpoint for products

--- src/main/java/com/example/minishop/product/ProductController.java
+++ src/main/java/com/example/minishop/product/ProductController.java
@@ -1,9 +1,13 @@
 package com.example.minishop.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -15,8 +19,12 @@ public class ProductController {
   private final ProductRepository productRepository;
 
   @GetMapping("/api/products")
-  public Iterable<Product> getAll() {
-    return productRepository.findAll();
+  public Page<Product> getAll(@RequestParam(required = false) String name,
+      @PageableDefault(size = 10, sort = "id") Pageable pageable) {
+    if (name != null && !name.isEmpty()) {
+      return productRepository.findByName(name, pageable);
+    }
+    return productRepository.findAll(pageable);
   }
 
   @GetMapping("/api/products/{id}")
--- src/main/java/com/example/minishop/product/ProductRepository.java
+++ src/main/java/com/example/minishop/product/ProductRepository.java
@@ -1,6 +1,9 @@
 package com.example.minishop.product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+  Page<Product> findByName(String name, Pageable pageable);
 }
